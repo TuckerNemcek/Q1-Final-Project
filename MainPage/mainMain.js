39da8f955c492ae9c664a50eafdd4d25ca58a4d6
Tucker Nemcek <tuckernemcek21@gmail.com>
2018-11-06 11:22:40 -0700

    almost have mvp. Main page is very close to working

--- MainPage/mainMain.js
+++ MainPage/mainMain.js
@@ -1,32 +1,50 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-localStorage.getItem('challenge')
-let name = localStorage.getItem('name')
-// let goal =
-if (localStorage.getItem('challenge') === 'First Challenge') {
-  title.innerText = "Hey "+name+", how are those pushups going?"
-  text_inline.innerText = "I bet it was a lot!"
-  chalAsk.innerText = "How many pushups did you do today?"
+  localStorage.getItem('challenge')
+  let achievedGoals =  document.getElementById("text_inline").value
 
+
+  let name = localStorage.getItem('name')
+  let goal = JSON.parse(localStorage.getItem('goal'))
+// goal sucessfully decrements when the event listener is put here
+
+
+  if (localStorage.getItem('challenge') === 'First Challenge') {
+    title.innerText = "Hey " + name + ", how are those pushups going?"
+    text_inline.innerText = "I bet it was a lot!"
+    chalAsk.innerText = "How many pushups did you do today?"
+
+
+  } else if (localStorage.getItem('challenge') === 'Second Challenge') {
+    title.innerText = "Hey " + name + ", how are those runs going?"
+    text_inline.innerText = "I bet you were fast!"
+    chalAsk.innerText = "How long did you run today?"
+
+
+  } else if (localStorage.getItem('challenge') === 'Third Challenge') {
+    title.innerText = "Hey " + name + ", how are those situps going?"
+    text_inline.innerText = "I bet your tummy hurts!"
+    chalAsk.innerText = "How many situps did you do today?"
+  }
+let body = document.querySelector("body")
+  let weekGoal = document.createElement("weekGoal")
+  weekGoal.innerHTML = `<div id="weekGoal" class="center-align">
+    only ${goal} left to go!
+  </div>`
+
+body.appendChild(weekGoal)
+
+function chalSubmit (){
+  achievedGoals = text_inline.value
 }
 
-else if (localStorage.getItem('challenge') === 'Second Challenge') {
-  title.innerText = "Hey "+name+", how are those runs going?"
-  text_inline.innerText = "I bet you were fast!"
-  chalAsk.innerText = "How long did you run today?"
-}
+submit.addEventListener("click", function(){
+  achievedGoals = goalInput.value
+  goal = goal - achievedGoals
+console.log(achievedGoals)
+console.log(goal)
 
-else if (localStorage.getItem('challenge') === 'Third Challenge') {
-  title.innerText = "Hey "+name+", how are those situps going?"
-  text_inline.innerText = "I bet your tummy hurts!"
-  chalAsk.innerText = "How many situps did you do today?"
-}
-// let tr = document.createElement("tr")
-// tr.innerHTML =  `<th scope="row">${elements[i].number}</th>
-//        <td>${elements[i].symbol}</td>
-//        <td>${elements[i].name}</td>`
-//         let tbody = document.querySelector("tbody")
-//
-// tbody.appendChild(tr)
 
+ })
+// DOMContentLoaded
 });
